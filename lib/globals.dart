ced90fcc6d4806eadb12a367e92b371452b89970
Pashanko <pnenchyn@gmail.com>
2023-09-13 20:09:50 +0300

    Fixed minor issues in flutter analyze

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,17 @@
 library globals;
 
+///Used in HomePage class to determine how far bottomSheet has to open to
 const double bottomOverlayDividedBy = 1.3;
+
+///Used in ColorHistoryList class to determine height of single item
 const double colorHistoryItemHeight = 48;
+
+///Used in ColorHistoryList class to determine font size of single item
 const double colorHistoryItemFontSize = 24;
+
+///Used in ColorHistoryList class to determine top margin between list and
+///the top of BottomSheet
 const double colorHistoryListTopMargin = 20;
+
+///Used in Main class to determine font size of greeting message
 const double fontSize24 = 24;
